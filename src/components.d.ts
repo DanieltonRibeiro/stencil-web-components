88feebe0612bb5e3fc789501e0a09140088806fb
Danielton <q.interesse@gmail.com>
2025-02-09 01:15:46 -0300

    feat(component): cria componente de contagem regressiva

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,9 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    interface AppCountime {
+        "time": string;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -22,6 +25,12 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLAppCountimeElement extends Components.AppCountime, HTMLStencilElement {
+    }
+    var HTMLAppCountimeElement: {
+        prototype: HTMLAppCountimeElement;
+        new (): HTMLAppCountimeElement;
+    };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
@@ -29,10 +38,14 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "app-countime": HTMLAppCountimeElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    interface AppCountime {
+        "time"?: string;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -48,6 +61,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "app-countime": AppCountime;
         "my-component": MyComponent;
     }
 }
@@ -55,6 +69,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "app-countime": LocalJSX.AppCountime & JSXBase.HTMLAttributes<HTMLAppCountimeElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
---
+++ src/components/app-countime/app-countime.tsx
@@ -0,0 +1,74 @@
+import { Component, Prop, State, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-countime',
+  styleUrl: 'app-countime.css',
+  shadow: true,
+})
+export class AppCountime {
+
+  @Prop() time: string;
+  @State() remainingTime: string = '';
+  private timer: any;
+
+  componentWillLoad() {
+    this.calculateRemainingTime();
+  }
+
+  componentDidLoad() {
+    this.startTimer();
+  }
+
+  componentWillUnload() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  
+  private calculateRemainingTime() {
+    const targetDate = new Date(this.time);
+    const currentDate = new Date();
+    const difference = targetDate.getTime() - currentDate.getTime();
+
+    if (difference <= 0) {
+      this.remainingTime = '00:00:00:00';
+      return;
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    this.remainingTime = `${this.formatTime(days)}:${this.formatTime(hours)}:${this.formatTime(minutes)}:${this.formatTime(seconds)}`;
+  }
+
+  
+  private formatTime(time: number): string {
+    return time < 10 ? `0${time}` : `${time}`;
+  }
+
+  
+  private startTimer() {
+    this.timer = setInterval(() => {
+      this.calculateRemainingTime();
+    }, 1000);
+  }
+
+  render() {
+    const [days, hours, minutes, seconds] = this.remainingTime.split(':');
+
+    return (
+      <div>
+    
+        <span>{days}</span><span>D </span>
+         : <span>{hours}</span><span>H </span>
+         : <span>{minutes}</span><span>M </span>
+         : <span>{seconds}</span><span>S</span>
+
+      
+      </div>
+    );
+  }
+}
